ae9fd1a2a6556ad892ed9f24f3e65a174984966b
Nerea García <nereagarcia.c@gmail.com>
2021-03-11 09:54:18 +0100

    Crear función borrarNumero

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { useEffect, useState } from "react";
 const numerosApi = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 function App() {
-  const [numeros, setNumeros] = useState(numerosApi);
+
+  // const [numeros, setNumeros] = useState(numerosApi);
   const [llamando, setLlamando] = useState(false);
+  const [numero, setNumero] = useState("657295390");
+
 
   const llamarOColgar = (e) => {
     e.preventDefault();
@@ -16,9 +19,18 @@ function App() {
     }
   };
 
-  const marcarNumero = () => {
-    console.log("hola");
+
+
+
+  const borrarNumero = () => {
+    setNumero(numero.slice(0, -1));
   };
+
+
+  /*   const marcarNumero = () => {
+      console.log("hola");
+    }; */
+
   const activoSiTiene9 = cifra => cifra.length === 9 ? " activo" : "";
 
   /*  Lo dejo porque quiero preguntar a Mario por que no hacerlo asi */
@@ -36,15 +48,15 @@ function App() {
         <div className="botones">
           <ol className="teclado">
             {
-              numeros.map(numero =>
+              numerosApi.map(numero =>
                 <li key={numero} className="numeros">
-                  <button disabled={llamando} onClick={() => marcarNumero()}>{numero}</button></li>)
+                  <button disabled={llamando}>{numero}</button></li>)
             }
-            <li><button disabled={llamando} className="big">borrar</button></li>
+            <li><button onClick={borrarNumero} disabled={llamando} className="big">borrar</button></li>
           </ol>
         </div>
         <div className="acciones">
-          <span className="numero">667359961</span>
+          <span className="numero">{numero}</span>
           {/*   <!-- El botón de llamar debe tener la clase "activo" cuando -->
                 <!-- el número de teléfono tiene 9 cifras --> */}
           {llamando ?
